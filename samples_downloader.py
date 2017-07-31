cd58f3d0b514aab7008c93ff167ca49c659462ce
phrasen <Lung-Yu@users.noreply.github.com>
2017-07-31 09:32:09 +0800

    update wget shell script

--- samples_downloader.py
+++ samples_downloader.py
@@ -6,8 +6,9 @@ BASE_URL = "http://malwaredb.malekal.com/"
 URL_MALEKAL = "http://malwaredb.malekal.com/index.php?page="
 
 
-def downloadFile(file_url):
-	shell = 'wget ' + file_url
+def downloadFile(file_url,filename):
+	shell = 'wget ' + file_url + " -O ./samples/malekal/" + filename+".zip"
+	print shell
 	os.system(shell)
 
 def getPage(pageIndex):
@@ -22,8 +23,9 @@ def getPage(pageIndex):
 	#get all file
 	for i in range(0,len(tds),+8):
 		file = tds[i].findAll('a')[0]['href']
-
-		downloadFile(BASE_URL + file)
+		filename = file[17:] 
+		url = BASE_URL + file[1:]
+		downloadFile(url,filename)
 
 def main():
 	for i in range(1,61):
